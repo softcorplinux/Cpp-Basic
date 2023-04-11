f8b1e4c7eefecfb0b319a93c4fffaefb4221832b
isaikin.viktor <isaikin.viktor@otr.ru>
2023-04-11 18:36:52 +0600

    init

---
+++ 01. Variables/main.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+#include "string"
+
+int main() {
+    int index = 100;
+    std::cout << index << std::endl;
+
+    std::string s = "Victor";
+    std::cout << s << '\n';
+
+    std::string last_name = "Isaikin";
+
+    return 0;
+}
