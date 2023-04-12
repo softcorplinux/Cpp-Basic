c63db472b8c8d7c3546dc58c94f80e5d33465bae
isaikin.viktor <isaikin.viktor@otr.ru>
2023-04-12 13:32:59 +0600

    01. variables

--- 01. variables/cmake-build-debug/cmake_install.cmake
+++ 01. variables/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/voj/Courses/Cpp/Basic/01. Variables
+# Install script for directory: /home/voj/Courses/Cpp/Basic/01. variables
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/voj/Courses/Cpp/Basic/01. Variables/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/voj/Courses/Cpp/Basic/01. variables/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- 01. variables/main.cpp
+++ 01. variables/main.cpp
@@ -2,13 +2,16 @@
 #include "string"
 
 int main() {
-    int index = 100;
-    std::cout << index << std::endl;
+    int age = 40;
+    std::cout << age << std::endl;
 
     std::string s = "Victor";
     std::cout << s << '\n';
 
+    std::cout << sizeof(s) << std::endl;
+
     std::string last_name = "Isaikin";
+
 
     return 0;
 }
