b594c8a693a18488613e9f2c59e8afa9b17ab7c1
isaikin.viktor <isaikin.viktor@otr.ru>
2023-04-19 16:00:50 +0600

    08. switch

---
+++ 08. switch/main.cpp
@@ -0,0 +1,38 @@
+/**
+ * Created by Victor Isaikin
+ */
+#include <iostream>
+#include "string"
+
+using namespace std;
+
+int main() {
+    cout << "Menu\n"
+        << "1. Cake\n"
+        << "2. Eggs\n"
+        << "3. Tomatoes\n"
+        << "4. Pasta" << endl;
+    cout << "Select the number: ";
+    char menu;
+    string s = "Victor";
+    cin >> menu;
+
+    switch (menu) { // 3
+        case 1:
+            cout << "Selected Cake\n";
+            [[fallthrough]];
+        case 2:
+        case 3:
+            cout << "Selected Tomatoes\n";
+            break;
+        case 4:
+            cout << "Selected Pasta\n";
+            break;
+        default:
+            cout << "You are not selected menu\n";
+            break;
+    }
+
+
+    return 0;
+}
