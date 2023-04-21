7e476b4e993da1fe7a2b17882ffe7eaeecec2f56
isaikin.viktor <isaikin.viktor@otr.ru>
2023-04-21 17:49:02 +0600

    11. for

---
+++ 11. for/main.cpp
@@ -0,0 +1,26 @@
+/**
+ * Created by Victor Isaikin
+ */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for(int index = 0; index < 4; index++){
+        cout << "Book " << index << endl;
+    }
+
+//    int index = 0;
+//    for(; index < 4;){
+//        cout << "Book " << index << endl;
+//        index++;
+//    }
+
+//    int num = 100;
+//
+//    for(int index = 0, age = 40; index < 4 && age > 20; index++, age -= 10, num += 20){
+//        cout << "Book " << index << " Age: " << age << " Num: " << num << endl;
+//    }
+
+    return 0;
+}
